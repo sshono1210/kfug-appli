7a71d243bde2f5032416181faef222a9922e0187
sshono <s.shosho1210@gmail.com>
2016-09-27 12:50:34 +0900

    とりあえずトップページ終わり

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var browser = require("browser-sync");
 
+// gulp：watchが走る
 gulp.task("default", ["watch"]);
 
+// gulp build：sassが走る
+gulp.task("build", ["sass"]);
+
+// gulp sass：コンパイルされる
 gulp.task("sass", function () {
     gulp.src("./frontend/assets/sass/**/*.scss")
         .pipe(sass())
@@ -11,10 +16,12 @@ gulp.task("sass", function () {
 
 });
 
-//gulp.task("watch", function(){
-//    gulp.watch(["./frontend/assets/sass/**/*.scss"], ["sass"]);
-//});
+// gulp watch：変更されるとコンパイルする
+gulp.task("watch", function(){
+    gulp.watch(["./frontend/assets/sass/**/*.scss"], ["sass"]);
+});
 
+// gulp server：ブラウザがたつ＆watchが走る
 gulp.task("server", function(){
     browser.init({
         server: {
