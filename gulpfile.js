313946317327eec3764b070e07eca688ac13c24f
sshono <s.shosho1210@gmail.com>
2016-09-27 11:29:00 +0900

    package.json、.gitignore、gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browser = require("browser-sync");
 
-gulp.task("default", function(){
-    console.log("hoge");
-});
+gulp.task("default", ["watch"]);
+
 gulp.task("sass", function () {
     gulp.src("./frontend/assets/sass/**/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("./public/css"))
 
+});
+
+//gulp.task("watch", function(){
+//    gulp.watch(["./frontend/assets/sass/**/*.scss"], ["sass"]);
+//});
+
+gulp.task("server", function(){
+    browser.init({
+        server: {
+            baseDir: "./public"
+        },
+        open: "external"
+    });
+    gulp.watch([
+        "./frontend/assets/sass/**/*.scss"
+    ], function(){
+        setTimeout(function(){
+            browser.reload();
+        }, 500);
+    })
 });
